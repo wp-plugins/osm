e29251e2d41254492cf1d24dca7e6ceda92391f2
photoweblog <photoweblog@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-24 04:49:01 +0000

    part of V0.9.1
    git-svn-id: https://plugins.svn.wordpress.org/osm/trunk@244855 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- osm-openlayers.php
+++ osm-openlayers.php
@@ -23,11 +23,11 @@ class Osm_OpenLayers
     $Layer .= '            controls:[';
     if (($a_MapControl[0] != 'off') && (strtolower($a_Type)!= 'ext')) {
       $Layer .= '              new OpenLayers.Control.Navigation(),';
-      $Layer .= '              new OpenLayers.Control.PanZoom()';
-     // $Layer .= '              new OpenLayers.Control.Attribution()';
+      $Layer .= '              new OpenLayers.Control.PanZoom(),';
+      $Layer .= '              new OpenLayers.Control.Attribution()';
     }
     else if (($a_MapControl[0] == 'off') && (strtolower($a_Type)!= 'ext')){
-      //$Layer .= '              new OpenLayers.Control.Attribution()';
+      $Layer .= '              new OpenLayers.Control.Attribution()';
     }
     else if (($a_MapControl[0] != 'off') && (strtolower($a_Type)== 'ext')){
       $Layer .= '              new OpenLayers.Control.Navigation(),';
