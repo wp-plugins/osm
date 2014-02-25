b5f877c7f9fd33161e0b08a65b03d3e851d313f8
photoweblog <photoweblog@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-25 17:16:54 +0000

    V2.6 (missed file)
    git-svn-id: https://plugins.svn.wordpress.org/osm/trunk@864854 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/OSM/openlayers/OpenStreetMap.js
+++ js/OSM/openlayers/OpenStreetMap.js
@@ -20,8 +20,9 @@ OpenLayers.Layer.OSM.Mapnik = OpenLayers.Class(OpenLayers.Layer.OSM, {
         ];
         options = OpenLayers.Util.extend({
             numZoomLevels: 20,
-            attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors",
+            attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors; <a href='http://wp-osm-plugin.HanBlog.Net'>OSM Plugin</a>",
             buffer: 0,
+            tileOptions: {crossOriginKeyword: null},
             transitionEffect: "resize"
         }, options);
         var newArguments = [name, url, options];
@@ -53,8 +54,9 @@ OpenLayers.Layer.OSM.CycleMap = OpenLayers.Class(OpenLayers.Layer.OSM, {
         ];
         options = OpenLayers.Util.extend({
             numZoomLevels: 19,
-            attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors, Tiles courtesy of <a href='http://www.opencyclemap.org'>Andy Allan</a>",
+            attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors, Tiles courtesy of <a href='http://www.opencyclemap.org'>Andy Allan</a>; <a href='http://wp-osm-plugin.HanBlog.Net'>OSM Plugin</a>",
             buffer: 0,
+            tileOptions: {crossOriginKeyword: null},
             transitionEffect: "resize"
         }, options);
         var newArguments = [name, url, options];
@@ -64,6 +66,73 @@ OpenLayers.Layer.OSM.CycleMap = OpenLayers.Class(OpenLayers.Layer.OSM, {
     CLASS_NAME: "OpenLayers.Layer.OSM.CycleMap"
 });
 
+
+/**
+ * Class: OpenLayers.Layer.OSM.StamenWC
+ *
+ * Inherits from:
+ *  - <OpenLayers.Layer.OSM>
+ */
+OpenLayers.Layer.OSM.StamenWC = OpenLayers.Class(OpenLayers.Layer.OSM, {
+    /**
+     * Constructor: OpenLayers.Layer.OSM.StamenWC
+     *
+     * Parameters:
+     * name - {String}
+     * options - {Object} Hashtable of extra options to tag onto the layer
+     */
+    initialize: function(name, options) {
+        var url = [
+            "http://a.tile.stamen.com/watercolor/${z}/${x}/${y}.jpg",
+            "http://b.tile.stamen.com/watercolor/${z}/${x}/${y}.jpg",
+            "http://c.tile.stamen.com/watercolor/${z}/${x}/${y}.jpg"
+        ];
+        options = OpenLayers.Util.extend({
+            numZoomLevels: 19,
+            attribution: "<a href=\"http://map.stamen.com\">Map tiles</a> by <a href=\"http://stamen.com\">Stamen Design</a>, under <a href=\"http://creativecommons.org/licenses/by/3.0\">CC BY 3.0</a>. Data by <a href=\"http://openstreetmap.org\">OpenStreetMap</a> and <a href=\"http://wp-osm-plugin.HanBlog.net\">OSM Plugin</a>",
+            buffer: 0,
+            tileOptions: {crossOriginKeyword: null},
+            transitionEffect: "resize"
+        }, options);
+        var newArguments = [name, url, options];
+        OpenLayers.Layer.OSM.prototype.initialize.apply(this, newArguments);
+    },
+
+    CLASS_NAME: "OpenLayers.Layer.OSM.StamenWC"
+});
+/**
+ * Class: OpenLayers.Layer.OSM.StamenToner
+ *
+ * Inherits from:
+ *  - <OpenLayers.Layer.OSM>
+ */
+OpenLayers.Layer.OSM.StamenToner = OpenLayers.Class(OpenLayers.Layer.OSM, {
+    /**
+     * Constructor: OpenLayers.Layer.OSM.StamenToner
+     *
+     * Parameters:
+     * name - {String}
+     * options - {Object} Hashtable of extra options to tag onto the layer
+     */
+    initialize: function(name, options) {
+        var url = [
+            "http://a.tile.stamen.com/toner/${z}/${x}/${y}.jpg",
+            "http://b.tile.stamen.com/toner/${z}/${x}/${y}.jpg",
+            "http://c.tile.stamen.com/toner/${z}/${x}/${y}.jpg"
+        ];
+        options = OpenLayers.Util.extend({
+            numZoomLevels: 19,
+            attribution: "<a href=\"http://map.stamen.com\">Map tiles</a> by <a href=\"http://stamen.com\">Stamen Design</a>, under <a href=\"http://creativecommons.org/licenses/by/3.0\">CC BY 3.0</a>. Data by <a href=\"http://openstreetmap.org\">OpenStreetMap</a> and <a href=\"http://wp-osm-plugin.HanBlog.net\">OSM Plugin</a>",
+            buffer: 0,
+            tileOptions: {crossOriginKeyword: null},
+            transitionEffect: "resize"
+        }, options);
+        var newArguments = [name, url, options];
+        OpenLayers.Layer.OSM.prototype.initialize.apply(this, newArguments);
+    },
+
+    CLASS_NAME: "OpenLayers.Layer.OSM.StamenToner"
+});
 /**
  * Class: OpenLayers.Layer.OSM.TransportMap
  *
